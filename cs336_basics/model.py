68580b22b9eb7a9feefd9e251626fcb2191001b7
dong <1057762929@qq.com>
2026-08-16 16:59:39 +0800

    实现linear 和 embedding 并测试通过

---
+++ cs336_basics/model.py
@@ -0,0 +1,31 @@
+import torch
+from torch import nn
+from einops import einsum, rearrange
+import math
+
+class Linear(nn.Module):
+    def __init__(self, in_features, out_features, device=None, dtype=None):
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = nn.Parameter(torch.empty(out_features, in_features, device=device, dtype=dtype))
+
+        ## 参数初始化
+        std = math.sqrt(2 / (in_features + out_features))
+
+        nn.init.trunc_normal_(self.weight, mean=0, std=std, a=-3*std, b=3*std)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return einsum(x, self.weight, "... d_in, d_out d_in -> ... d_out")
+
+class Embedding(nn.Module):
+    def __init__(self, num_embeddings, embedding_dim, device=None, dtype=None):
+        super().__init__()
+        self.num_embeddings = num_embeddings
+        self.embedding_dim = embedding_dim
+        self.weight = nn.Parameter(torch.empty(num_embeddings, embedding_dim, device=device, dtype=dtype))
+
+        nn.init.trunc_normal_(self.weight, mean=0, std=1, a=-3, b=3)
+
+    def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
+        return self.weight[token_ids]
--- tests/adapters.py
+++ tests/adapters.py
@@ -10,6 +10,7 @@ from jaxtyping import Bool, Float, Int
 from torch import Tensor
 
 from cs336_basics.tokenizer import train_bpe, Tokenizer
+from cs336_basics.model import Linear, Embedding
 
 
 
@@ -31,9 +32,9 @@ def run_linear(
     Returns:
         Float[Tensor, "... d_out"]: The transformed output of your linear module.
     """
-
-    raise NotImplementedError
-
+    linear = Linear(d_in, d_out)
+    linear.load_state_dict({"weight": weights})
+    return linear(in_features)
 
 def run_embedding(
     vocab_size: int,
@@ -53,8 +54,9 @@ def run_embedding(
     Returns:
         Float[Tensor, "... d_model"]: Batch of embeddings returned by your Embedding layer.
     """
-
-    raise NotImplementedError
+    embedding = Embedding(vocab_size, d_model)
+    embedding.load_state_dict({"weight": weights})
+    return embedding(token_ids)
 
 
 def run_swiglu(
