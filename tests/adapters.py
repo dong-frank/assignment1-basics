4f0f03f6ac70e7fb91fd9189485cb3e52b004bc7
dong <1057762929@qq.com>
2026-08-19 20:48:38 +0800

    实现 RMSNorm 并通过测试

--- tests/adapters.py
+++ tests/adapters.py
@@ -11,6 +11,7 @@ from torch import Tensor
 
 from cs336_basics.tokenizer import train_bpe, Tokenizer
 from cs336_basics.model import Linear, Embedding
+from cs336_basics.nn_utils import RMSNorm
 
 
 
@@ -383,7 +384,9 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
+    rmsnorm = RMSNorm(d_model, eps)
+    rmsnorm.load_state_dict({"weight": weights})
+    return rmsnorm(in_features)
 
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
