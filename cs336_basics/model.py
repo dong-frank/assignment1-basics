de2953314e0d301ced58b5c990e5b19177a48009
dong <1057762929@qq.com>
2026-08-22 17:20:55 +0800

    实现RoPE 并通过测试

--- cs336_basics/model.py
+++ cs336_basics/model.py
@@ -61,13 +61,54 @@ class SwiGLU(nn.Module):
         super().__init__()
 
         self.d_model = d_model
+        if d_ff is None:
+            times = round((8 / 3 * self.d_model) / 64)
+            self.d_ff = times * 64
+            assert self.d_ff % 64 == 0
 
-        times = round((8 / 3 * self.d_model) / 64)
-        self.d_ff = d_ff or times * 64
+        else:
+            self.d_ff = d_ff
 
+        if self.d_ff < 1:
+            raise ValueError("d_ff must be at least 1, got 0")
+        
         self.w1 = Linear(self.d_model, self.d_ff, device=device, dtype=dtype)
         self.w2 = Linear(self.d_ff, self.d_model, device=device, dtype=dtype)
         self.w3 = Linear(self.d_model, self.d_ff, device=device, dtype=dtype)
 
     def forward(self, x:torch.Tensor) -> torch.Tensor:
         return self.w2(silu(self.w1(x)) * self.w3(x))
+
+class RotaryPositionalEmbedding(nn.Module):
+    def __init__(self, theta: float, d_k: int, max_seq_len: int, device=None):
+        super().__init__()
+
+        self.theta = theta
+        self.d_k = d_k
+
+        j_list = torch.arange(start=0, end=d_k // 2, dtype=torch.float32, device=device)
+        f_k = theta ** -(2 * j_list / d_k)
+        place_list = torch.arange(start=0, end=max_seq_len, dtype=torch.float32, device=device)
+        degree = einsum(place_list, f_k, "i, j -> i j")
+
+        self.register_buffer("cos", torch.cos(degree), persistent=False)
+        self.register_buffer("sin", torch.sin(degree), persistent=False)
+
+    def forward(self, x: torch.Tensor, token_positions: torch.Tensor) -> torch.Tensor:
+        cos_table = self.cos[token_positions]
+        sin_table = self.sin[token_positions]
+        x_pairs = rearrange(x, "... seq (pairs two) -> ... seq pairs two", pairs=self.d_k // 2, two=2)
+        a = x_pairs[..., 0]
+        b = x_pairs[..., 1]
+        a_rotate = a * cos_table - b * sin_table
+        b_rotate = a * sin_table + b * cos_table
+
+        x_pairs_rotate = torch.stack([a_rotate, b_rotate], dim=-1)
+
+        x = rearrange(x_pairs_rotate, "... seq pairs two -> ... seq (pairs two)", pairs= self.d_k // 2, two=2)
+
+        return x
+
+
+if __name__ == "__main__":
+    swiglu = SwiGLU(d_model=8)
--- tests/adapters.py
+++ tests/adapters.py
@@ -10,7 +10,7 @@ from jaxtyping import Bool, Float, Int
 from torch import Tensor
 
 from cs336_basics.tokenizer import train_bpe, Tokenizer
-from cs336_basics.model import Linear, Embedding, RMSNorm, SwiGLU
+from cs336_basics.model import Linear, Embedding, RMSNorm, SwiGLU, RotaryPositionalEmbedding
 from cs336_basics.nn_utils import softmax, silu
 
 
@@ -208,7 +208,8 @@ def run_rope(
     Returns:
         Float[Tensor, " ... sequence_length d_k"]: Tensor with RoPEd input.
     """
-    raise NotImplementedError
+    rope = RotaryPositionalEmbedding(theta, d_k, max_seq_len)
+    return rope(in_query_or_key, token_positions)
 
 
 def run_transformer_block(
